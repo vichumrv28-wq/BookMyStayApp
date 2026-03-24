fd2f1dae00ef6a9fe59e1a2f64a4d2473c160f1e
vichumrv28-wq <vichumrv28@gmail.com>
2026-03-24 10:05:05 +0530

    uc1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,18 @@
+/**
+ * uc1
+ * Entry point for Hotel Booking Application
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println(" Welcome to Hotel Booking System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("======================================");
+
+        System.out.println("Application started successfully.");
+    }
+}
